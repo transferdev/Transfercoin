e93d6787a7dcf8a8759bfac5ef51171dc134ca12
Infernoman <jjaauto@hotmail.com>
2015-10-13 13:53:22 -0400

    build fixes

--- src/version.cpp
+++ src/version.cpp
@@ -37,7 +37,6 @@ const std::string CLIENT_NAME("Inferno");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID "b9993cd"
-#    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
