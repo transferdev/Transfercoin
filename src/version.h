ea0d99bcf977138cd36c981ee58c79958f20e349
Infernoman <jjaauto@hotmail.com>
2015-10-13 22:38:55 -0400

    no blocks from version 61401 and above

--- src/version.h
+++ src/version.h
@@ -48,7 +48,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60020;
-static const int NOBLKS_VERSION_END = 61400;
+static const int NOBLKS_VERSION_END = 61401;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
