193076db41a9549666505c93d39b195cd27878a1
Infernoman <jjaauto@hotmail.com>
2015-10-18 13:33:39 -0400

    mn -> activeMasternode

--- src/qt/masternodemanager.cpp
+++ src/qt/masternodemanager.cpp
@@ -160,14 +160,14 @@ void MasternodeManager::updateNodeList()
 
         // populate list
         // Address, Rank, Active, Active Seconds, Last Seen, Pub Key
-        QTableWidgetItem *activeItem = new QTableWidgetItem(QString::number(mn.IsEnabled()));
-        QTableWidgetItem *addressItem = new QTableWidgetItem(QString::fromStdString(mn.addr.ToString()));
-        QTableWidgetItem *rankItem = new QTableWidgetItem(QString::number(mnodeman.GetMasternodeRank(mn.vin, pindexBest->nHeight)));
-        QTableWidgetItem *activeSecondsItem = new QTableWidgetItem(seconds_to_DHMS((qint64)(mn.lastTimeSeen - mn.now)));
-        QTableWidgetItem *lastSeenItem = new QTableWidgetItem(QString::fromStdString(DateTimeStrFormat(mn.lastTimeSeen)));
+        QTableWidgetItem *activeItem = new QTableWidgetItem(QString::number(activeMasternode.IsEnabled()));
+        QTableWidgetItem *addressItem = new QTableWidgetItem(QString::fromStdString(activeMasternode.addr.ToString()));
+        QTableWidgetItem *rankItem = new QTableWidgetItem(QString::number(mnodeman.GetMasternodeRank(activeMasternode.vin, pindexBest->nHeight)));
+        QTableWidgetItem *activeSecondsItem = new QTableWidgetItem(seconds_to_DHMS((qint64)(activeMasternode.lastTimeSeen - mn.now)));
+        QTableWidgetItem *lastSeenItem = new QTableWidgetItem(QString::fromStdString(DateTimeStrFormat(activeMasternode.lastTimeSeen)));
 	
         CScript pubkey;
-        pubkey =GetScriptForDestination(mn.pubkey.GetID());
+        pubkey =GetScriptForDestination(activeMasternode.pubkey.GetID());
         CTxDestination address1;
         ExtractDestination(pubkey, address1);
         CBitcoinAddress address2(address1);
