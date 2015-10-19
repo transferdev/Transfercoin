4466bd3566bfe6eb5dc206a61ccbb476f876e3af
Infernoman <jjaauto@hotmail.com>
2015-10-19 11:57:38 -0400

    Revert "Few fixes for processing extra messages:"
    This reverts commit 886dc3eeb4cf5145c6d63a8b2cf8fd29a2e66022.

--- src/main.cpp
+++ src/main.cpp
@@ -3999,7 +3999,6 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 
         ProcessMessageDarksend(pfrom, strCommand, vRecv);
         mnodeman.ProcessMessage(pfrom, strCommand, vRecv);
-        ProcessMessageMasternodePayments(pfrom, strCommand, vRecv);
         ProcessMessageInstantX(pfrom, strCommand, vRecv);
         ProcessSpork(pfrom, strCommand, vRecv);
 
