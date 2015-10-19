a86dfb613267902074bd4c2733650c173a51498c
Infernoman <jjaauto@hotmail.com>
2015-10-18 20:52:08 -0400

    fix

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -167,7 +167,7 @@ void CMasternodeMan::CheckAndRemove()
             ++it;
         }
     }
-}
+
 
     // check who's asked for the masternode list
     map<CNetAddr, int64_t>::iterator it1 = mAskedUsForMasternodeList.begin();
@@ -192,7 +192,6 @@ void CMasternodeMan::CheckAndRemove()
             it2 = mWeAskedForMasternodeListEntry.erase(it2);
         else ++it2;
     }
-
 }
 
 int CMasternodeMan::CountEnabled()
