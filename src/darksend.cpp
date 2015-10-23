87982a4e22ba2ed8b1dcdec5059d9127ef1ec756
Infernoman <jjaauto@hotmail.com>
2015-10-23 06:21:43 -0400

    test

--- src/darksend.cpp
+++ src/darksend.cpp
@@ -2065,16 +2065,19 @@ bool CDarkSendSigner::VerifyMessage(CPubKey pubkey, vector<unsigned char>& vchSi
     ss << strMessageMagic;
     ss << strMessage;
 
+    pubkey = GetScriptForDestination(pubkey.GetID());
+    pubkey2 = GetScriptForDestination(pubkey.GetID());
+
     CPubKey pubkey2;
     if (!pubkey2.RecoverCompact(ss.GetHash(), vchSig)) {
         errorMessage = _("Error recovering public key.");
         return false;
     }
 
-    if (fDebug && pubkey2.GetID() != pubkey.GetID())
-        LogPrintf("CDarkSendSigner::VerifyMessage -- keys don't match: %s '%s'\n", pubkey2.GetID().ToString(), pubkey.GetID().ToString());
+    if (fDebug && pubkey2 != pubkey)
+        LogPrintf("CDarkSendSigner::VerifyMessage -- keys don't match: %s '%s'\n", pubkey2.ToString(), pubkey.ToString());
 
-    return (pubkey2.GetID() == pubkey.GetID());
+    return (pubkey2 == pubkey);
 }
 
 bool CDarksendQueue::Sign()
