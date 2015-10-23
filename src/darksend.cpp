f2e31ca29df3f3a67c574f5abcbf879ddd74085d
Infernoman <jjaauto@hotmail.com>
2015-10-23 07:43:49 -0400

    Revert "test fixes darksend"
    This reverts commit e85c06a7ce591c1018c126c533918c5e13eac88b.

--- src/darksend.cpp
+++ src/darksend.cpp
@@ -2066,20 +2066,15 @@ bool CDarkSendSigner::VerifyMessage(CPubKey pubkey, vector<unsigned char>& vchSi
     ss << strMessage;
 
     CPubKey pubkey2;
-    CScript test;
-    CScript test2;
     if (!pubkey2.RecoverCompact(ss.GetHash(), vchSig)) {
         errorMessage = _("Error recovering public key.");
         return false;
     }
 
-    test = GetScriptForDestination(pubkey.GetID());
-    test2 = GetScriptForDestination(pubkey2.GetID());
+    if (fDebug && pubkey2.GetID() != pubkey.GetID())
+        LogPrintf("CDarkSendSigner::VerifyMessage -- keys don't match: %s '%s'\n", pubkey2.GetID().ToString(), pubkey.GetID().ToString());
 
-    if (fDebug && test != test2)
-        LogPrintf("CDarkSendSigner::VerifyMessage -- keys don't match: %s '%s'\n", test2, test);
-
-    return (test2 == test);
+    return (pubkey2.GetID() == pubkey.GetID());
 }
 
 bool CDarksendQueue::Sign()
