7d43884bd623875e9cf63b7e7f61d6be3cea5c67
Infernoman <jjaauto@hotmail.com>
2015-11-18 21:29:42 -0500

    cutoff old nodes

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 61402;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 61401;
+static const int MIN_PEER_PROTO_VERSION = 61402;
 
 // minimum peer version accepted by DarkSendPool
 static const int MIN_POOL_PEER_PROTO_VERSION = 61402; 
