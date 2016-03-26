78e7d6b2552b9bfe7963640aed9e03e708518838
Infernoman <jjaauto@hotmail.com>
2016-03-26 00:07:45 -0700

    Removing un-needed include
    - remove an un-needed include for mswsock.h as we use winsock2.h anyway
    
    https://github.com/bitcoin/bitcoin/pull/2857/commits/dd833a4cd887bda58b8686a61287c342e9c56003

--- src/compat.h
+++ src/compat.h
@@ -3,7 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef _BITCOIN_COMPAT_H
-#define _BITCOIN_COMPAT_H 1
+#define _BITCOIN_COMPAT_H
 
 #ifdef WIN32
 #ifdef _WIN32_WINNT
@@ -22,7 +22,6 @@
 #define FD_SETSIZE 1024 // max number of fds in fd_set
 
 #include <winsock2.h>
-#include <mswsock.h>
 #include <ws2tcpip.h>
 #else
 #include <sys/fcntl.h>
